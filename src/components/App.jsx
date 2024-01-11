93ecbbf6320a2fbf14f59dd3378e86f69fac2773
Viktoriia <128598090+kamineckav@users.noreply.github.com>
2024-01-11 12:43:30 +0100

    completed all homework assignments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import User from './User/User';
+import user from '../data/user.json';
+import Statistics from './Statistics/Statistics';
+import statistics from '../data/data.json';
+import FriendList from './FriendList/FriendList';
+import friends from '../data/friends.json';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import transactions from '../data/transactions.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <main>
+      <User items={user} />
+      <Statistics title="UPLOAD STATS" items={statistics} />
+      <FriendList items={friends} />
+      <TransactionHistory items={transactions} />
+    </main>
   );
 };
---
+++ src/components/User/User.jsx
@@ -0,0 +1,53 @@
+import css from './User.module.css'
+import PropTypes from 'prop-types';
+
+
+const User = ({ items }) => {
+  return (
+    <>
+      <div className={css.profile}>
+        <div className={css.description}>
+          <img src={items.avatar} alt="User avatar" className={css.avatar} />
+          <p className={css.username}>{items.username}</p>
+          <p className={css.tag}>@{items.tag}</p>
+          <p className={css.location}>{items.location}</p>
+        </div>
+
+        <ul className={css.stats}>
+          <li className={css.statsItem}>
+            <span className={css.label}>Followers:</span>
+            <span className={css.quantity}>{items.stats.followers}</span>
+          </li>
+          <li className={css.statsItem}>
+            <span className={css.label}>Views:</span>
+            <span className={css.quantity}>{items.stats.views}</span>
+          </li>
+          <li className={css.statsItem}>
+            <span className={css.label}>Likes:</span>
+            <span className={css.quantity}>{items.stats.likes}</span>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default User;
+
+User.defaultProps = {
+  items: [],
+};
+
+User.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      followers: PropTypes.number,
+      views: PropTypes.number,
+      likes: PropTypes.number,
+    })
+  ),
+};
